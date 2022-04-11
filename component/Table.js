eb3cac8bcb0ee947a57d80840163458a4c996362
FreeCoderKR <cystmax@gmail.com>
2022-04-12 08:16:37 +0900

    FEAT : 가게 메인페이지 데이터 페칭로직과 모달 필드 데이터 입력부분 작업

--- component/Table.js
+++ component/Table.js
@@ -1,9 +1,8 @@
 export default function Table({ datalist }) {
-  console.log(Object.keys(datalist[0]), Object.values(datalist[0]));
+  // console.log(Object.keys(datalist[0]), Object.values(datalist[0]));
 
   return (
     <div>
-      <div>Table</div>
       <table>
         <thead>
           <tr>
--- pages/shop/[id].js
+++ pages/shop/[id].js
@@ -1,17 +1,20 @@
 import styles from "./Shop.module.css";
 import { useRouter } from "next/router";
 import axios from "axios";
-import { useState } from "react";
-
+import { useRef, useState } from "react";
+import Table from "../../component/Table";
+import Modal from "../../component/Modal";
 export default function Shop(props) {
   const router = useRouter();
   const { id } = router.query;
-  console.log(props);
+
   const [shop, setShop] = useState(props.data.shop);
   const [customs, setCustoms] = useState(props.data.customs);
   const [customers, setCustomers] = useState(props.data.customers);
   const [products, setProducts] = useState(props.data.products);
   const [orders, setOrders] = useState(props.data.orders);
+  const [openModal, setOpenModal] = useState(false);
+  const modalInfo = useRef({});
 
   const updateCustom = async (idx, custom) => {};
   const deleteCustom = async (idx, custom) => {
@@ -20,6 +23,37 @@ export default function Shop(props) {
       let newList = customs.filter((data) => data._id !== custom._id);
       setCustoms(newList);
     }
+  };
+  const refetchData = async () => {
+    console.log("refetchData");
+    const result = await axios.get(`/api/shops/${id}`);
+    if (result.data?.success) {
+      let shopData = result.data.data?.shops;
+      //TODO : useSWR로 실시간 데이터 페칭
+      setCustoms(shopData.customs);
+      setCustomers(shopData.customs);
+      setProducts(shopData.products);
+      setOrders(shopData.orders);
+    }
+  };
+  const onClickCreate = async (item) => {
+    if (item === "customs") {
+      //custom create 고정
+      modalInfo.current = {
+        fields: ["store", "collectionName", "fieldName", "fieldType"],
+        defaultData: { store: id },
+        collection: item,
+      };
+    } else {
+      //products
+      let productCustoms = customs.filter((custom) => custom.collectionName === item);
+      modalInfo.current = {
+        fields: ["store", "name", "price", "categories", ...productCustoms],
+        defaultData: { store: id },
+        collection: item,
+      };
+    }
+    setOpenModal(true);
   };
   return (
     <div className={styles.root}>
@@ -34,7 +68,9 @@ export default function Shop(props) {
       </div>
       <div className={styles.dataContainer}>
         <div>Custom Field List</div>
-        <button style={{ marginBottom: "20px" }}>Create Custom Field</button>
+        <button style={{ marginBottom: "20px" }} onClick={() => onClickCreate("customs")}>
+          Create Custom Field
+        </button>
         {customs.map((custom, idx) => (
           <div className={styles.listItems} key={custom.fieldName}>
             <div style={{ width: "30px", textAlign: "center" }}>{idx + 1}</div>
@@ -45,14 +81,30 @@ export default function Shop(props) {
         ))}
       </div>
       <div className={styles.dataContainer}>
-        <div>This is Our Customer</div>
+        <div>This is Our Product</div>
+        <button style={{ marginBottom: "20px" }} onClick={() => onClickCreate("products")}>
+          Create Product
+        </button>
+        {products.length > 0 ? <Table datalist={products} /> : <div>There are no Products</div>}
       </div>
       <div className={styles.dataContainer}>
-        <div>This is Our Product</div>
+        <div>This is Our Customer</div>
+        {customers.length > 0 && <Table datalist={customers} />}
       </div>
+
       <div className={styles.dataContainer}>
         <div>This is Our Order</div>
+        {orders.length > 0 && <Table datalist={orders} />}
       </div>
+      {openModal && (
+        <Modal
+          setOpenModal={setOpenModal}
+          fields={modalInfo.current.fields}
+          defaultData={modalInfo.current.defaultData}
+          collection={modalInfo.current.collection}
+          callback={refetchData}
+        />
+      )}
     </div>
   );
 }
