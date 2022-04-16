2ba29116808b3436f5dfd32d0b16c0851c92ffc2
FreeCoderKR <cystmax@gmail.com>
2022-04-16 21:58:56 +0900

    server.js 수정

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const next = require("next");
 const port = 3000;
-
+const dotenv = require("dotenv");
+dotenv.config();
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const apiRouter = require("./router/apiRouter");
 const { swaggerUi, specs } = require("./lib/swagger");
 const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config();
-const dev = process.env.NODE_ENV !== "production";
+
 app.prepare().then(() => {
   const server = express();
   server.use(cors());
