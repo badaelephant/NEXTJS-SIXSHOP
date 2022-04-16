75ba2b4cec99c1781f4588a590a275f346606f34
FreeCoderKR <cystmax@gmail.com>
2022-04-16 21:36:18 +0900

    mongo

--- middlewares/database.js
+++ middlewares/database.js
@@ -2,7 +2,7 @@
 
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb://localhost:27017";
+const uri = process.env.MONGO;
 const options = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
